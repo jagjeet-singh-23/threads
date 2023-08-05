404c8e04cc65bb8e1584ef6e6eadeb9ff029c7fc
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-05 19:07:39 +0530

    modified:   components/shared/Topbar.tsx 	new file:   public/assets/community.svg 	new file:   public/assets/create.svg 	new file:   public/assets/delete.svg 	new file:   public/assets/edit.svg 	new file:   public/assets/heart-filled.svg 	new file:   public/assets/heart-gray.svg 	new file:   public/assets/heart.svg 	new file:   public/assets/home.svg 	new file:   public/assets/logo.svg 	new file:   public/assets/logout.svg 	new file:   public/assets/members.svg 	new file:   public/assets/more.svg 	new file:   public/assets/profile.svg 	new file:   public/assets/reply.svg 	new file:   public/assets/repost.svg 	new file:   public/assets/request.svg 	new file:   public/assets/search-gray.svg 	new file:   public/assets/search.svg 	new file:   public/assets/share.svg 	new file:   public/assets/tag.svg 	new file:   public/assets/user.svg

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 const Topbar = () => {
   return (
-    <div>Topbar</div>
-  )
-}
+    <nav className="topbar">
+      <Link href="/" className="flex items-center gap-4">
+        <Image src="/assets/logo.svg" alt="logo" height={28} width={28} />
+        <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+      </Link>
+    </nav>
+  );
+};
 
-export default Topbar
+export default Topbar;
