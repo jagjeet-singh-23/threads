1d61eeb985e01536e11d0db4667b6a4c697f1e70
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-05 20:00:09 +0530

    renamed:    app/favicon.ico -> app/icon.ico 	modified:   components/shared/LeftSidebar.tsx 	new file:   constants/index.ts

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
-
 const LeftSidebar = () => {
   return (
-    <div>LeftSidebar</div>
-  )
-}
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6"></div>
+    </section>
+  );
+};
 
-export default LeftSidebar
+export default LeftSidebar;
