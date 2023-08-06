fb9eee7072a495e17dc453550e31ca89cc810f89
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-06 15:23:00 +0530

    new file:   app/(auth)/onboarding/page.tsx 	modified:   app/globals.css 	new file:   components.json 	new file:   components/forms/AccountProfile.tsx 	modified:   components/shared/Topbar.tsx 	new file:   components/ui/button.tsx 	new file:   components/ui/form.tsx 	new file:   components/ui/input.tsx 	new file:   components/ui/label.tsx 	new file:   components/ui/textarea.tsx 	new file:   lib/utils.ts 	new file:   lib/validations/user.ts 	modified:   package-lock.json 	modified:   package.json

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,7 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import { dark } from "@clerk/themes";
 
 const Topbar = () => {
   return (
@@ -27,6 +27,7 @@ const Topbar = () => {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
