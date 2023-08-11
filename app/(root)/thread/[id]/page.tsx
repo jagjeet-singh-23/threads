c26bc3946ecce2504c4c3353112fff5d5530c9bd
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-11 20:32:19 +0530

    modified:   app/(root)/thread/[id]/page.tsx 	modified:   components/cards/ThreadCard.tsx

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -34,12 +34,29 @@ const ThreadDetails = async ({ params }: { params: { id: string } }) => {
           comments={thread.children}
         />
       </div>
+
       <div className="mt-7">
         <Comment
           threadId={thread.id}
-          currentUserImg={user.imageUrl}
+          currentUserImg={userInfo.image}
           currentUserId={JSON.stringify(userInfo._id)}
         />
+      </div>
+
+      <div className="mt-10">
+        {thread.children.map((childItem: any) => (
+          <ThreadCard
+            id={childItem._id}
+            currentUserId={childItem?.id || ""}
+            parentId={childItem.parentId}
+            content={childItem.text}
+            author={childItem.author}
+            community={childItem.community}
+            createdAt={childItem.createdAt}
+            comments={childItem.children}
+            isComment
+          />
+        ))}
       </div>
     </section>
   );
