0e5665eebcb48a0bbf02ba18fda28912edc42fa9
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-14 19:34:11 +0530

    modified:   app/(auth)/layout.tsx 	modified:   app/(root)/activity/page.tsx 	modified:   lib/actions/user.action.ts

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,8 +1,60 @@
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+import {
+  fetchNotifications,
+  fetchUser,
+  fetchUsers,
+} from "@/lib/actions/user.action";
+import Image from "next/image";
+import Link from "next/link";
+
 const ActivityPage = async () => {
+  const user = await currentUser();
+  if (!user) {
+    return null;
+  }
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) {
+    redirect("/onboarding");
+  }
+
+  // get Notifications
+  const notifications = await fetchNotifications(userInfo._id);
+
   return (
-    <section>
+    <>
       <h1 className="head-text mb-10">Activity</h1>
-    </section>
+      <section className="mt-10 flex flex-col gap-5">
+        {notifications.length > 0 ? (
+          <>
+            {notifications.map((notification) => (
+              <Link
+                key={notification._id}
+                href={`/thread/${notification.parentId}`}
+              >
+                <article className="activity-card">
+                  <Image
+                    src={notification.author.image}
+                    alt="user image"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {notification.author.name}
+                    </span>{" "}
+                    has replied to your thread.
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet.</p>
+        )}
+      </section>
+    </>
   );
 };
 
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -121,3 +121,27 @@ export async function fetchUserPosts(userId: string) {
     throw new Error(`Failed to fetch user posts: ${error.message}`);
   }
 }
+
+export async function fetchNotifications(userId: string) {
+  try {
+    connectToDB();
+    // find all the threads created by this user.
+    const userThreads = await Thread.find({ author: userId });
+    // get all the child threadIds from the 'children' field
+    const childThreadIds = userThreads.reduce((acc, userThread) => {
+      return acc.concat(userThread.children);
+    }, []);
+    const replies = await Thread.find({
+      _id: { $in: childThreadIds },
+      author: { $ne: userId },
+    }).populate({
+      path: "author",
+      model: User,
+      select: "name image _id",
+    });
+
+    return replies;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch notifications: ${error.message}`);
+  }
+}
