482a36a6ec47b0c1790411b573486bc2501bb879
jagjeet-singh-23 <125492254+jagjeet-singh-23@users.noreply.github.com>
2023-08-14 17:57:08 +0530

    modified:   components/cards/UserCard.tsx

--- components/cards/UserCard.tsx
+++ components/cards/UserCard.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Button } from "../ui/button";
+
 interface UserCardProps {
   id: string;
   name: string;
@@ -13,7 +19,30 @@ const UserCard = ({
   imgUrl,
   personType,
 }: UserCardProps) => {
-  return <div>UserCard</div>;
+  const router = useRouter();
+  return (
+    <article className="user-card">
+      <div className="user-card_avatar">
+        <Image
+          src={imgUrl}
+          alt="User Avatar"
+          width={48}
+          height={48}
+          className="rounded-full"
+        />
+        <div className="flex-1 text-ellipsis">
+          <h4 className="text-base-semibold text-light-1">{name}</h4>
+          <p className="text-small-medium text-gray-1">@{username}</p>
+        </div>
+      </div>
+      <Button
+        className="user-card_btn"
+        onClick={() => router.push(`/profile/${id}`)}
+      >
+        View
+      </Button>
+    </article>
+  );
 };
 
 export default UserCard;
